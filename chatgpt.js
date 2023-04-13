afb22ba61a17453ee037b3d3509bd290fa4735c2
profradchenko <130706239+profradchenko@users.noreply.github.com>
2023-04-13 23:22:06 +0100

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -1,16 +1,22 @@
-export default async function submitForm(prompt) {
-  const webAppUrl = "https://script.google.com/macros/s/AKfycbxZsKFZKsdkrruv55HglpbOMtZk5NDWmyoGjxwgb4JxVOOE2FUT6nEBy0RxxfNCvm_g/exec";
-  const fetchUrl = `${webAppUrl}?prompt=${encodeURIComponent(prompt)}`;
+const chatgpt = (() => {
+  async function submitForm(prompt) {
+    const webAppUrl = "https://script.google.com/macros/s/AKfycbxZsKFZKsdkrruv55HglpbOMtZk5NDWmyoGjxwgb4JxVOOE2FUT6nEBy0RxxfNCvm_g/exec";
+    const fetchUrl = `${webAppUrl}?prompt=${encodeURIComponent(prompt)}`;
 
-  try {
-    const response = await fetch(fetchUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error ${response.status}: ${response.statusText}`);
+    try {
+      const response = await fetch(fetchUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}: ${response.statusText}`);
+      }
+      const text = await response.text();
+      return text;
+    } catch (error) {
+      console.error("Error:", error);
+      throw new Error(`An error occurred: ${error.message}. Please try again.`);
     }
-    const text = await response.text();
-    return text;
-  } catch (error) {
-    console.error("Error:", error);
-    throw new Error(`An error occurred: ${error.message}. Please try again.`);
   }
-}
+
+  return {
+    submitForm: submitForm
+  };
+})();
